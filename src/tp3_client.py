768a69c10f9e459cd41806d769392fbc7adb099f
ealmansi <ealmansi@gmail.com>
2014-11-11 12:37:35 -0300

    agrego sleep antes de cerrar conexion

--- src/tp3_client.py
+++ src/tp3_client.py
@@ -4,18 +4,21 @@
 import os
 import ptc
 from tp3_config import SERVER_IP, SERVER_PORT, TRANSFER_DATA, TRANSFER_CHUNK_SIZE
+import time
 
 def main():
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again using 'sudo'. Exiting.")
 
     with ptc.Socket() as client_sock:
-        client_sock.connect((SERVER_IP, SERVER_PORT), timeout = 20)
+        client_sock.connect((SERVER_IP, SERVER_PORT), timeout = 30)
+        client_sock.shutdown(ptc.SHUT_WR)
         data_received = ''
         while len(data_received) < len(TRANSFER_DATA):
           data_received += client_sock.recv(TRANSFER_CHUNK_SIZE)
           print len(data_received), ' out of ', len(TRANSFER_DATA), 'bytes received.\r',
         print ''
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
--- src/tp3_server.py
+++ src/tp3_server.py
@@ -4,6 +4,7 @@
 import os
 import ptc
 from tp3_config import SERVER_IP, SERVER_PORT, TRANSFER_DATA, TRANSFER_CHUNK_SIZE
+import time
 
 def main():
     if os.geteuid() != 0:
@@ -12,7 +13,7 @@ def main():
     with ptc.Socket() as server_sock:
         server_sock.bind((SERVER_IP, SERVER_PORT))
         server_sock.listen()
-        server_sock.accept(timeout = 20)
+        server_sock.accept(timeout = 30)
         data_index = 0
         while data_index < len(TRANSFER_DATA):
           chunk_size = min(len(TRANSFER_DATA) - data_index, TRANSFER_CHUNK_SIZE)
@@ -21,6 +22,7 @@ def main():
           data_index = data_index + chunk_size
           print data_index, ' out of ', len(TRANSFER_DATA), 'bytes sent.\r',
         print ''
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()

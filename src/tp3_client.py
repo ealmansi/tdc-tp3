ad77cf36fcbeff10f3b44d0feaa0e9b55467f4c2
fcanay <fcanay@hotmail.com>
2014-11-13 17:42:11 -0300

    fruta para ver si funcca

--- src/tp3_client.py
+++ src/tp3_client.py
@@ -9,6 +9,7 @@ import time
 def receive_data(client_sock, ack_drop_rate = None, ack_delay = None, ack_delay_var = 0):
     client_sock.protocol.set_ack_drop_rate(ack_drop_rate)
     client_sock.protocol.set_ack_delay(ack_delay,ack_delay_var)
+    
     data_received = ''
     while len(data_received) < len(TRANSFER_DATA):
       chunk_size = min(len(TRANSFER_DATA) - len(data_received), TRANSFER_CHUNK_SIZE)
@@ -20,14 +21,14 @@ def main():
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again using 'sudo'. Exiting.")
     port = SERVER_PORT
-    for (_,_,delay,delay_var,drop) in TEST_RTO_PARAMS:
-        with ptc.Socket() as client_sock:
-            print 'Client socket open.'
-            client_sock.connect((SERVER_IP, port), timeout = 30)
-            port = port + 1
+      with ptc.Socket() as client_sock:
+          print 'Client socket open.'
+          client_sock.connect((SERVER_IP, port), timeout = 30)
+            #port = port + 1
+          for (_,_,delay,delay_var,drop) in TEST_RTO_PARAMS:
             receive_data(client_sock, drop, delay,delay_var)
             time.sleep(1)
-        print 'Client socket closed.'
+      print 'Client socket closed.'
 
 if __name__ == '__main__':
     main()
--- src/tp3_server.py
+++ src/tp3_server.py
@@ -8,6 +8,10 @@ import time
 
 def send_data(server_sock, alpha, beta):
   server_sock.protocol.set_rto_estimation_parameters(alpha, beta)
+  server_sock.protocol.srtt = 0
+  server_sock.protocol.rttvar = 0
+  server_sock.protocol.rto = INITIAL_RTO
+    
   data_index = 0
   while data_index < len(TRANSFER_DATA):
     chunk_size = min(len(TRANSFER_DATA) - data_index, TRANSFER_CHUNK_SIZE)
@@ -22,13 +26,13 @@ def main():
         exit("You need to have root privileges to run this script.\nPlease try again using 'sudo'. Exiting.")
     print 5
     port = SERVER_PORT
-    for (alpha, beta,_,_,_) in TEST_RTO_PARAMS:
-        with ptc.Socket() as server_sock:
-            #print 'Server socket open.'
-            server_sock.bind((SERVER_IP, port))
-            port = port + 1
-            server_sock.listen()
-            server_sock.accept(timeout = 30)
+      with ptc.Socket() as server_sock:
+          #print 'Server socket open.'
+          server_sock.bind((SERVER_IP, port))
+          #port = port + 1
+          server_sock.listen()
+          server_sock.accept(timeout = 30)
+          for (alpha, beta,_,_,_) in TEST_RTO_PARAMS:
             print '%f %f' % (alpha, beta)
             send_data(server_sock, alpha, beta)
         #print 'Server socket closed.'
